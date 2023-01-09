6f1777314d3dac6446702cafc89b1bba6c49c57c
ng-dao-29 <112545575+ng-dao-29@users.noreply.github.com>
2023-01-09 12:38:44 +0700

    commit

--- server/dist/index.js
+++ server/dist/index.js
@@ -1,36 +1,34 @@
-import express from 'express'
-import http from "http";
-
-const app = express();
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const http_1 = __importDefault(require("http"));
+const app = (0, express_1.default)();
 const { Server } = require("socket.io");
-const httpServer = http.createServer(app);
+const httpServer = http_1.default.createServer(app);
 const PORT = 3500;
-
-
 const io = new Server(httpServer, {
     cors: {
         origin: "http://localhost:3000",
         methods: ["GET", "POST"],
     },
 });
-
 io.on("connection", (socket) => {
     console.log(`User Connected: ${socket.id}`);
-
     socket.on("join_room", (data) => {
         socket.join(data);
         console.log(`User with ID: ${socket.id} joined room: ${data}`);
     });
-
     socket.on("send_message", (data) => {
         socket.to(data.room).emit("receive_message", data);
     });
-
     socket.on("disconnect", () => {
         console.log("User Disconnected", socket.id);
     });
 });
-
 httpServer.listen(PORT, () => {
     console.log(`server listening on port ${PORT}`);
 });
+//# sourceMappingURL=index.js.map
